78e23f53590019616eb393455743d398b8d72769
Maibram <166405330+Maibram@users.noreply.github.com>
2024-07-10 08:37:09 +0530

    php

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "products";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+<?php
+
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "products";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
---
+++ php2.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $num1 = 5;
+    $num2 = 10;
+    $result = $num1 * num2;
+    echo "result is: " .$result;
+?>
